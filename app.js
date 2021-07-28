faa376cc020400b2d7e8d6d2973a45d24fc3859c
Tyler Scott <off.nadir.insight@gmail.com>
2021-07-27 20:31:26 -0600

    add update to functionality

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const btns = document.getElementsByClassName('button')
+const btns = document.getElementsByClassName('button');
 let numA = 0;
 let numB = 0;
 let operator;
+let displayValue = "0";
+let secondaryDisplayValue = "0";
+const display = document.getElementById("display--numbers");
+const secondaryDisplay = document.getElementById("secondary-display");
+
+const operatorFunctions = {
+  add: function(num1, num2) {
+    return num1 + num2;
+  },
+  sub: function(num1, num2) {
+    return num1 - num2;
+  },
+  mul: function (num1, num2) {
+    return num1 * num2;
+  },
+  div: function (num1, num2) {
+    return (num1 / num2).toFixed(7);
+  }
+}
 
 // handles css styling for clicking buttons
 Array.from(btns).forEach(btn => btn.addEventListener('mousedown', event => {
@@ -16,69 +35,52 @@ Array.from(btns).forEach(btn => btn.addEventListener('mouseleave', event => {
 
 // capture button clicks
 Array.from(btns).forEach(btn => btn.addEventListener('click', event => {
-  // console.log(Number(event.target.dataset.btnValue))
   handleBtnClick(event)
 }));
 
-let displayValue = ""
-const display = document.getElementById("display--numbers")
-
 function updateDisplay() {
   display.textContent = displayValue;
+  secondaryDisplay.textContent = secondaryDisplayValue;
 }
-
-
-const operatorFunctions = {
-  add: function(num1, num2) {
-    return num1 + num2;
-  },
-  sub: function(num1, num2) {
-    return num1 - num2;
-  },
-  mul: function (num1, num2) {
-    return num1 * num2;
-  },
-  div: function (num1, num2) {
-    return num1 / num2
-  }
-}
-
-// function operate(mathFunc, num1, num2) {
-//   return mathFunc(num1, num2)
-// }
 
 function clearScreen() {
-  displayValue = ""
+  displayValue = "0";
+  secondaryDisplayValue = "0"
   numA = 0;
   numB = 0;
-  updateDisplay()
+  operator = null;
+  updateDisplay();
 }
 
 function handleBtnClick(event) {
   const {btnValue} = event.target.dataset
-  console.log(btnValue)
 
   if (btnValue === "C") {
     clearScreen()
   }
-  if (btnValue === "add" || btnValue === "sub" || btnValue === "mul" || btnValue === "div") {
-    if (numA) {
-      numB = Number(displayValue);
-      displayValue = operatorFunctions[operator](numA, numB);
-      updateDisplay();
-      console.log(display.textContent)
-      numB = 0;
-      return;
+
+  if (btnValue === "=") {
+    numB = Number(displayValue);
+    if (Object.keys(operatorFunctions).includes(operator)){
+      displayValue = operatorFunctions[operator](numA, numB)
     }
+    numA = 0;
+    numB = 0;
+    operator = null;
+    updateDisplay();
+  }
+
+  if (btnValue === "add" || btnValue === "sub" || btnValue === "mul" || btnValue === "div") {
     numA = Number(displayValue);
     operator = btnValue;
-    console.log(numA, operator, numB)
-    
-    displayValue = "";
-
+    secondaryDisplayValue = `${numA} ${event.target.textContent}`;
+    displayValue = "0";
   }
-  if (displayValue.length < 10 && btnValue <= 9 && btnValue >= 0) {
-    displayValue += event.target.dataset.btnValue
+
+  if (displayValue === "0" && btnValue <= 9 && btnValue >= 0) {
+    displayValue = event.target.dataset.btnValue;
+  } else if (displayValue.length < 10 && btnValue <= 9 && btnValue >= 0) {
+    displayValue += event.target.dataset.btnValue;
   }
   updateDisplay()
 }
